4c1d0690c229f43f1ae45c4c924959b914c006a7
Afsar Ali <mohammedafsar.ali@cognizant.com>
2021-07-25 21:36:12 +0800

    Commit missed files

---
+++ src/screens/Details/Details.js
@@ -0,0 +1,16 @@
+import React,{Component} from "react";
+import './Details.css'
+
+class Details extends Component{
+
+    render() {
+        return(
+            <div>
+                This is Details Page
+            </div>
+
+        )
+    }
+}
+
+export default Details;
